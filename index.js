692fd878de8e8ccc9d63d6480107277e16348841
Dani <d.tarazoma@geekshubs.com>
2022-06-29 11:08:07 +0200

    feat: static database

--- index.js
+++ index.js
@@ -4,4 +4,25 @@ const app = express();
 
 const port = process.env.PORT || 3000
 
+// database 
+let movies = [
+    {
+        id: 1,
+        title: "El padrino"
+    },
+    {
+        id: 2,
+        title: "Spiderman"
+    },
+    {
+        id: 3,
+        title: "Soy leyenda"
+    },
+    {
+        id: 4,
+        title: "El ultimo samurai"
+    },
+
+];
+
 app.listen(port, () => console.log("Server is running on port " + port))
